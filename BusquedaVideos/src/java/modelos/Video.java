33f433ee1ea0aaaf36f18980f54591d36ecd7cae
Beji94 <benat.jimenez1994@gmail.com>
2018-04-24 10:56:52 +0200

    BusquedaVideosSOAP

---
+++ BusquedaVideos/src/java/modelos/Video.java
@@ -0,0 +1,133 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelos;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author Beñat
+ */
+public class Video {
+    
+    private String titulo;
+    private String autor;
+    private Date fecha;
+    private String fechaxml;
+    private String duracion;
+    private int reproduccion;
+    private String descripcion;
+    private String formato;
+    private String url;
+    private String idUsuario;
+    private int apubli;
+
+
+    
+    public Video(String titulo, String autor, Date fecha, String duracion, int reproduccion, String descripcion, String formato, String url, String idUsuario, int apubli ) {
+        this.titulo = titulo;
+        this.autor = autor;
+       // this.fecha = fecha;
+        this.duracion = duracion;
+        this.reproduccion = reproduccion;
+        this.descripcion = descripcion;
+        this.formato = formato;
+        this.url = url;
+        this.idUsuario = idUsuario;
+        this.apubli = apubli;
+        this.fechaxml=fecha.toString().replace("-", "/");
+    } 
+    
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha =  fecha;
+    }
+
+    public void setFechaXML(String fechaxml) {
+        this.fechaxml = fecha.toString();
+       
+        
+    }
+     public String getFechaXML() {
+            
+          return this.fechaxml.replace("-", "/");
+          
+    }
+    public String getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(String duracion) {
+        this.duracion = duracion;
+    }
+
+    public int getReproduccion() {
+        return reproduccion;
+    }
+
+    public void setReproduccion(int reproduccion) {
+        this.reproduccion = reproduccion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getFormato() {
+        return formato;
+    }
+
+    public void setFormato(String formato) {
+        this.formato = formato;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(String idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+    
+    public int getApubli() {
+        return apubli;
+    }
+
+    public void setApubli(int apubli) {
+        this.apubli = apubli;
+    }
+}
---
+++ BusquedaVideos/src/java/org/me/busqueda/BusquedaWS.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.me.busqueda;
+
+import java.sql.Date;
+import java.sql.SQLException;
+import java.util.Vector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.ejb.Stateless;
+import modelos.Video;
+import modelos.VideoDAO;
+
+/**
+ *
+ * @author Beñat
+ */
+@WebService(serviceName = "BusquedaWS")
+@Stateless()
+public class BusquedaWS {
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "busqueda")
+    public Vector<modelos.Video> busqueda(@WebParam(name = "autor") String autor, @WebParam(name = "titulo") String titulo, @WebParam(name = "apubliDesde") int apubliDesde, @WebParam(name = "apubliHasta") int apubliHasta) {
+        Vector<Video> listaVideo = new Vector<Video>();
+        VideoDAO vDAO = new VideoDAO();
+        try {
+            if(autor.equals(null)) autor = "%";
+            if(titulo.equals(null)) titulo = "%";
+            if(Integer.toString(apubliDesde).equals(null)) apubliDesde=0;
+            if(Integer.toString(apubliHasta).equals(null)) apubliHasta=0;
+            
+            listaVideo= vDAO.busqueda(autor, titulo, apubliDesde, apubliHasta, "bjimenez");
+        } catch (SQLException ex) {
+            Logger.getLogger(BusquedaWS.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return listaVideo;
+    }
+}
